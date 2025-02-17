ac933d08ad84b80ea31a5126fec61291d0b8e5d7
gentlsnek <erwinpimenta1644@gmail.com>
2025-02-17 11:32:47 +0530

    bit more

--- split.py
+++ split.py
@@ -1,12 +1,12 @@
 import subprocess
-from anachange import change_pitch
+#from anachange import change_pitch
 
 def split_audio(input_audio, output_folder="separated"):
     command = ["demucs"," --two-stems=vocals" ,input_audio,output_folder]
     subprocess.run(command)
     print(f"Separation complete! Check the '{output_folder}' folder.")
     
-    change_pitch(f"{output_folder}/vocals.wav", 2)
+   # change_pitch(f"{output_folder}/vocals.wav", 2)
     
     
 
